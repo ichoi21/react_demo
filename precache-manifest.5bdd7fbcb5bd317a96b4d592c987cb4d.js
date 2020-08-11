57cfec36c49a695ba3c570f570468136f36bf090
ichoi21 <ihchoi@gmail.com>
2020-08-10 19:29:49 -0700

    Updates

---
+++ precache-manifest.5bdd7fbcb5bd317a96b4d592c987cb4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476e65b1d0008ae6ba4e34bb5c39e865",
+    "url": "/react_demo/index.html"
+  },
+  {
+    "revision": "6cf8f05753f900e5f15d",
+    "url": "/react_demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d11dcc7fe82575edd17",
+    "url": "/react_demo/static/js/2.66b01075.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_demo/static/js/2.66b01075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf8f05753f900e5f15d",
+    "url": "/react_demo/static/js/main.eec6debb.chunk.js"
+  },
+  {
+    "revision": "64379e00f26a0751e3c7",
+    "url": "/react_demo/static/js/runtime-main.5b3b8743.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
